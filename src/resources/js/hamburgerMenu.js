8dc48dac39900d89ecbd220bb104cda1e0d48d19
milic178 <marko.milic178@gmail.com>
2024-10-16 23:07:54 +0200

    - fix js issue hamburgerMenu not loaded

--- src/resources/js/hamburgerMenu.js
+++ src/resources/js/hamburgerMenu.js
@@ -1,21 +1,22 @@
-// resources/js/hamburgerMenu.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const menuItems = document.getElementById('menu-items');
 
-    // Toggle menu visibility
-    hamburgerMenu.addEventListener('click', function (event) {
-        menuItems.classList.toggle('hidden');
-    });
+    // Check if hamburger menu and menu items exist
+    if (hamburgerMenu && menuItems) {
+        // Toggle menu visibility
+        hamburgerMenu.addEventListener('click', function () {
+            menuItems.classList.toggle('hidden');
+        });
 
-    // Close the menu when clicking outside
-    document.addEventListener('click', function (event) {
-        const isClickInsideMenu = menuItems.contains(event.target);
-        const isClickOnHamburger = hamburgerMenu.contains(event.target);
+        // Close the menu when clicking outside
+        document.addEventListener('click', function (event) {
+            const isClickInsideMenu = menuItems.contains(event.target);
+            const isClickOnHamburger = hamburgerMenu.contains(event.target);
 
-        if (!isClickInsideMenu && !isClickOnHamburger) {
-            menuItems.classList.add('hidden');
-        }
-    });
+            if (!isClickInsideMenu && !isClickOnHamburger) {
+                menuItems.classList.add('hidden');
+            }
+        });
+    }
 });
